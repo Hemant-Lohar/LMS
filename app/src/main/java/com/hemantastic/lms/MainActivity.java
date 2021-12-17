fb3fb838e0be0c06931f749f1123bbdf9c92c59a
Hemant-Lohar <hemantlohar585@gmail.com>
2021-12-17 17:42:08 +0530

    LMS

--- app/src/main/java/com/hemantastic/lms/MainActivity.java
+++ app/src/main/java/com/hemantastic/lms/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     protected void prt(){
-        System.out.println("commit test");
+        System.out.println("LMS");
     }
 }
