cc36a7b321b8bbb175e0e4d4e396813a506fb451
Sangram <sangramharihar007@gmail.com>
2021-12-19 16:37:29 +0530

    sangram Navigation menu created

--- app/src/main/java/com/hemantastic/lms/Home.java
+++ app/src/main/java/com/hemantastic/lms/Home.java
@@ -1,8 +1,15 @@
 package com.hemantastic.lms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,6 +17,13 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment
+                = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.mycourses,R.id.explore,R.id.profile).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
     }
 
