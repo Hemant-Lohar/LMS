88c520983c05c6c23e95c906683562cb42afe510
Sangram <sangramharihar007@gmail.com>
2021-12-20 12:18:13 +0530

    manifest file

--- app/src/main/java/com/hemantastic/lms/mycourses.java
+++ app/src/main/java/com/hemantastic/lms/mycourses.java
@@ -1,5 +1,6 @@
 package com.hemantastic.lms;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +21,7 @@ public class mycourses extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -39,21 +42,29 @@ public class mycourses extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static mycourses newInstance(String param1, String param2) {
         mycourses fragment = new mycourses();
+        
+
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
